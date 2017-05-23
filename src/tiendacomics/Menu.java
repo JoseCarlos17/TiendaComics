69aa61893dfe72ce979782f3473775a25f04dfad
AlumMati <AlumMati@PC11>
2017-05-23 14:12:50 +0200

    // Iconos

--- src/tiendacomics/Menu.java
+++ src/tiendacomics/Menu.java
@@ -27,10 +27,50 @@ public class Menu extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jButton1.setText("Compras");
+        getContentPane().add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 60, 110, 50));
+
+        jButton2.setText("Distribuidores");
+        getContentPane().add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 180, 110, 50));
+
+        jButton3.setText("Editorial");
+        getContentPane().add(jButton3, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 180, 110, 50));
+
+        jButton4.setText("Facturas");
+        getContentPane().add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 180, 110, 50));
+
+        jButton5.setText("Historial");
+        getContentPane().add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 300, 110, 50));
+
+        jButton6.setText("Peticiones");
+        getContentPane().add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 300, 110, 50));
+
+        jButton7.setText("Desconexión");
+        getContentPane().add(jButton7, new org.netbeans.lib.awtextra.AbsoluteConstraints(230, 440, 190, 50));
+
+        jButton8.setText("Ventas");
+        getContentPane().add(jButton8, new org.netbeans.lib.awtextra.AbsoluteConstraints(480, 300, 110, 50));
+
+        jButton9.setText("Cómics");
+        getContentPane().add(jButton9, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 60, 110, 50));
+
+        jButton10.setText("Clientes");
+        getContentPane().add(jButton10, new org.netbeans.lib.awtextra.AbsoluteConstraints(60, 60, 110, 50));
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/tiendacomics/imagenes/menuflash.jpg"))); // NOI18N
         jLabel1.setText("jLabel1");
@@ -75,6 +115,16 @@ public class Menu extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel1;
     // End of variables declaration//GEN-END:variables
 }
